84f826b8fe4157e828f1281bac24ef437a12e542
Jack Perry <jack.e.perry@outlook.com>
2021-07-02 09:31:49 -0600

    📦 NEW: expressions lesson completed

---
+++ rock-the-jvm-scala-beginners/src/lectures/part1Basics/Expressions.scala
@@ -0,0 +1,53 @@
+package lectures.part1Basics
+
+object Expressions extends App {
+  val x: Int = 1 + 2  // Expression
+  println(x)
+
+  println(2 + 3 * 4)
+  // + - * / & | ^ << >> >>> (right shift with zero extension)
+
+  println(1 == x)
+  // == != > >= < <=
+
+  println(!(1 == x))
+  // ! && ||
+
+  var aVariable: Int = 2
+  println(aVariable)
+  aVariable += 3  // += -= *= /=
+  println(aVariable)
+
+  // Instructions vs Expressions
+  // Instruction - something you tell computer to do
+  // Expression - something that has value and/or type
+
+  // IF expression
+  val aCondition: Boolean = true
+  val aConditionedValue = if(aCondition) 5 else 3
+  println(aConditionedValue)
+  println(if(aCondition) 5 else 3)
+
+  var i: Int = 0
+  while (i < 10) {
+    println(i)
+    i += 1
+  }
+  // NEVER WRITE THIS AGAIN ^
+
+  // Everything in Scala is an Expression
+
+  val aWeirdValue = (aVariable = 3)  // Unit == void
+  println(aWeirdValue)
+
+  // Code Blocks
+  val aCodeBlock = {
+    val y: Int = 2
+    val z: Int = y + 1
+
+    if (z > 2) "hello" else "goodbye"
+  }
+
+  //  val anotherValue  = z + 1
+  // Not within block, fails
+}
