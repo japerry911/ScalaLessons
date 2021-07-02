bd03c3db0ee517e49cc080a176a1f2d414861bbc
Jack Perry <jack.e.perry@outlook.com>
2021-07-02 09:13:17 -0600

    👌 IMPROVE: finished values variables types lesson

--- rock-the-jvm-scala-beginners/src/lectures/part1Basics/ValuesVariablesTypes.scala
+++ rock-the-jvm-scala-beginners/src/lectures/part1Basics/ValuesVariablesTypes.scala
@@ -4,5 +4,49 @@ object ValuesVariablesTypes extends App {
   val x: Int = 42
   println(x)
 
+  // vals are immutable
+  // types of vals are optional, compiler can infer type
+  val y = 42
+  println(y)
+
   val aString: String = "Hello"
+  println(aString)
+
+  val aBoolean: Boolean = true  // false
+  println(aBoolean)
+
+  val aChar: Char = 'a'
+  println(aChar)
+
+  val aInt: Int = 123
+  println(aInt)
+
+  val aShort: Short = 4613
+  println(aShort)
+
+  val aLong: Long = 1234567890L
+  println(aLong)
+
+  val aFloat: Float = 2.0f
+  println(aFloat)
+
+  val aDouble: Double = 3.14
+  println(aDouble)
+
+  // Variables || can be reassigned/mutable
+  var aVariable: Int = 4
+  println(aVariable)
+  aVariable = 5  // side effects
+  println(aVariable)
+
+  /*
+  Takeaways:
+    - prefer vals over vars
+    - all vals and vars have types
+    - compiler automatically infers types when omitted
+    - learned how to use basic types
+      - boolean
+      - int, long, double
+      - string
+   */
 }
