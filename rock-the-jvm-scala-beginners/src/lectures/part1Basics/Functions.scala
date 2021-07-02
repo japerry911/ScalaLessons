21a27ca31693b0ee2d3d85343e39dab66638cec2
Jack Perry <jack.e.perry@outlook.com>
2021-07-02 14:19:47 -0600

    👌 IMPROVE: exercises completed for Functions lesson

--- rock-the-jvm-scala-beginners/src/lectures/part1Basics/Functions.scala
+++ rock-the-jvm-scala-beginners/src/lectures/part1Basics/Functions.scala
@@ -1,5 +1,7 @@
 package lectures.part1Basics
 
+import scala.annotation.tailrec
+
 object Functions extends App {
   def aFunction(a: String, b: Int): String = {
     a + " " + b
@@ -37,4 +39,41 @@ object Functions extends App {
       f(n) = f(n - 1) + f(n - 2)
     4.) Function that tests if a number is prime
    */
+
+  // 1.)
+  def greetings(name: String, age: Int): Unit =
+    println("Hi, my name is " + name + " and I am " + age + " years old")
+  greetings("Jack", 100)
+
+  // 2.)
+  def factorial(n: Int): Int = if (n <= 1) n else n * factorial(n - 1)
+  println(factorial(10) + " == 3,628,800")
+
+  // 3.)
+  def fibonacci(n: Int): Int =
+    if (n <= 2) 1 else fibonacci(n - 1) + fibonacci(n - 2)
+  println(fibonacci(10) + " == 55")
+
+  // 4.)
+  // class version
+  def isPrime2(n: Int): Boolean = {
+    @tailrec
+    def isPrimeUntil(t: Int): Boolean =
+      if (t <= 1) false
+      else if (n % t == 0) false
+      else isPrimeUntil(t - 1)
+    isPrimeUntil(n / 2)
+  }
+
+  // my version
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def checkPrime(n: Int, i: Int): Boolean =
+      if (i >= n) true else if (n % i == 0) false else checkPrime(n, i + 1)
+
+    if (n <= 1) false else checkPrime(n, 2)
+  }
+  println("1 is Prime: False == " + isPrime(1))
+  println("17 is Prime: True == " + isPrime(17))
+  println("100 is Prime: False == " + isPrime(100))
 }
