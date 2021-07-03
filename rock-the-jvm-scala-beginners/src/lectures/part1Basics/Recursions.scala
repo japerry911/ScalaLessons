01d2120cd7a002dc0be4c91e2183f69c8a163f6e
Jack Perry <jack.e.perry@outlook.com>
2021-07-03 11:52:17 -0600

    👌 IMPROVE: recursions exercises completed

--- rock-the-jvm-scala-beginners/src/lectures/part1Basics/Recursions.scala
+++ rock-the-jvm-scala-beginners/src/lectures/part1Basics/Recursions.scala
@@ -43,8 +43,43 @@ object Recursions extends App {
 
   /*
   Exercises:
-    1.) Concatenate a String n times
+    1.) Concatenate a String n times, tail recursive
     2.) isPrime function that is tail recursive
     3.) Fibonacci function that is tail recursive
    */
+
+  // 1.)
+  @tailrec
+  def concatenateNTimes(aString: String, n: Int, returnString: String = ""): String =
+    if (n == 1) returnString
+    else concatenateNTimes(aString, n - 1, aString + returnString)
+
+  println(concatenateNTimes("Skylord", 10))
+
+  // 2.)
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeTailRec(t: Int, isStillPrime: Boolean): Boolean = {
+      if (!isStillPrime) false
+      else if (t <= 1) true
+      else isPrimeTailRec(t - 1, n % t != 0 && isStillPrime)
+    }
+
+    isPrimeTailRec(n / 2, isStillPrime = true)
+  }
+
+  println(isPrime(2003))  // True
+  println(isPrime(629))  // False
+
+  // 3.)
+  def fibonacci(n: Int): Int = {
+    @tailrec
+    def fiboTailRec(i: Int, last: Int, nextToLast: Int): Int =
+      if (i >= n) last
+      else fiboTailRec(i + 1, last + nextToLast, last)
+    if (n <= 2) 1
+    else fiboTailRec(2, 1, 1)
+  }
+
+  println(fibonacci(8))  // 21
 }
